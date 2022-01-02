4ccce9a141d2d4262785b4c3002ded9ffa6ce68f
wlopezob <wlopezob@gmail.com>
2022-01-02 13:21:17 -0500

    init

---
+++ src/main/java/com/app/init/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.app.init.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hola")
+    public String hola(){
+        return "hola";
+    }
+}
---
+++ src/test/java/com/app/init/InitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
