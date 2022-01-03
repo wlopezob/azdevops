d7fef0554c1810bc2e5b1bad8a96ef6be30e3687
wlopezob <wlopezob@hotmail.com>
2022-01-02 21:58:19 -0500

    delete

--- src/main/java/com/app/init/controller/HelloController.java
+++ src/main/java/com/app/init/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hola")
     public String hola(){
-        return "hola";
+        return "hola01";
     }
 }
