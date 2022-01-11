45758f47d3868ac475ff3d030a26736fb8622ebb
wlopezob <wlopezob@hotmail.com>
2022-01-11 10:32:48 -0500

    readme.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# CI-CD Azure Devops - EKS
+
+Este repositorio contiene un pequeño ejemplo con spring boot, vamos a desplegar la aplicacion en Amazon-EKS
+  
+
+## AzureDevops - Pipelines: Instalar las siguientes extensiones
+1. [AWS Toolkit for Azure DevOps](https://marketplace.visualstudio.com/items?itemName=AmazonWebServices.aws-vsts-tools)
+2. [Replace Tokens](https://marketplace.visualstudio.com/items?itemName=qetza.replacetokens)
+
+## Azure Pipelines: Crear variables:
+1. kube_config: contiene el valor base64 del archivo kube.config
+```
+cat ~/.kube/config | base64 > base64.txt
+//copiar el contenido del archivo base64.txt (borrar espacio en blancos)
+```
--- src/main/java/com/app/init/controller/HelloController.java
+++ src/main/java/com/app/init/controller/HelloController.java
@@ -1,11 +1,29 @@
 package com.app.init.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hola")
+    @Operation(
+            description = "retorna un hola",
+            method = "GET",
+            tags = {"persona"},
+            summary = "retorna un hola"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200", description = "retorna un hola"
+            ),
+    })
     public String hola(){
         return "hola03";
     }
