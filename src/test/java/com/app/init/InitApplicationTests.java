1c8fcbe821fcd76bdcc05f2ec7d2adb82eaa3463
william luis lopez <wlopezob@hotmail.com>
2022-01-13 21:01:47 +0000

    Updated InitApplicationTests.java

--- src/test/java/com/app/init/InitApplicationTests.java
+++ src/test/java/com/app/init/InitApplicationTests.java
@@ -1,9 +1,11 @@
 package com.app.init;
 
 import com.app.init.controller.HelloController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class InitApplicationTests {
